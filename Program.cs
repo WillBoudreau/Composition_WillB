fd4a63492cd288850ecd736623c5d1e5c7a9acdd
WillBoudreau <williamboudreau804@gmail.com>
2024-01-15 14:12:03 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition_WillB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Composition");
+            Console.WriteLine();
+
+
+            Console.WriteLine("PRess nay key to quit");
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Composition_WillB
+ 
